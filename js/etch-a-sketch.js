dde8629af74dca96bb52da3c879926c0de0f2985
Kurtis Berger <118536145+milkywaysquirreldeer@users.noreply.github.com>
2023-04-19 06:34:35 -0400

    Create grid resizing and sketch erase buttons, and add grid size info display

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,23 +1,57 @@
-const surface = document.querySelector('.etching-surface');
+// Makes all "pixels" responsive to mouse hover to enable etching functionality
+const enableGridPixels = function () { 
+  document.querySelectorAll('.grid-pixel').forEach(pixel =>
+   pixel.addEventListener('mouseover', () => pixel.classList.add('etched')));
+};
 
-/* Dynamically fill the etching surface with "pixels", depending on pixels-per-
-side value provided by user */
-generatePixels = function(pixelCountPerSide) {
-  const surfaceVhPercent = 70;
-  const pixelHeightWidth = surfaceVhPercent/pixelCountPerSide;
+/* Dynamically wipes existing "pixels" and refills the etching surface with new
+    ones, using a pixel count defined by user */
+const generateGridPixels = function(pixelCountPerSide) {
+  document.querySelectorAll('.grid-pixel').forEach(pixel => pixel.remove());
+  const gridSurfaceVhPercent = 70;
+  const pixelHeightWidth = gridSurfaceVhPercent/pixelCountPerSide;
   for (let i=1; i<=(pixelCountPerSide**2); i++) {
-    const surfaceUnit = document.createElement('div');
-    surfaceUnit.classList.add('surface-unit');
-    surfaceUnit.style.height = `${pixelHeightWidth}vh`;
-    surfaceUnit.style.width = `${pixelHeightWidth}vh`;
-    surface.appendChild(surfaceUnit);
+    const gridPixel = document.createElement('div');
+    gridPixel.classList.add('grid-pixel');
+    gridPixel.style.height = `${pixelHeightWidth}vh`;
+    gridPixel.style.width = `${pixelHeightWidth}vh`;
+    document.querySelector('.etching-grid-surface').appendChild(gridPixel);
+    enableGridPixels();
   }
-}
+};
 
-let userPixelsPerSide = 16;
-generatePixels(userPixelsPerSide);
+const defaultPixelsPerSide = 15;
+let currentPixelsPerSide = defaultPixelsPerSide;
 
-// Make the "pixels" responsive to mouse hover to enable etching functionality
-const surfaceUnits = document.querySelectorAll('.surface-unit');
-surfaceUnits.forEach(unit => unit.addEventListener('mouseover',
- () => unit.classList.add('etched')));
+generateGridPixels(defaultPixelsPerSide);
+
+const displayGridDimensions = function() {
+  const totalPixels = (currentPixelsPerSide ** 2);
+  document.querySelector('.grid-info-dynamic').textContent =
+   `${totalPixels} (${currentPixelsPerSide} x ${currentPixelsPerSide})`;
+};
+
+displayGridDimensions();
+
+/* Button functionality for user-defined grid size */
+const changeGridDimensions = function () {
+  const userPixelsPerSide = prompt(`Change grid size? There are currently
+   ${currentPixelsPerSide} drawing pixels per side (higher values will take
+   longer to load). Valid range is 2 - 30.`,
+   currentPixelsPerSide);
+  currentPixelsPerSide = userPixelsPerSide;
+  generateGridPixels(currentPixelsPerSide);
+  displayGridDimensions();
+};
+
+document.querySelector('.change-grid-size-button').addEventListener('click',
+ changeGridDimensions);
+
+/* Button functionality to erase current sketch */
+const eraseGrid = function() {
+  document.querySelectorAll('.etched').forEach(etchedPixel =>
+    etchedPixel.classList.remove('etched'));
+  };
+
+document.querySelector('.erase-grid-button').addEventListener('click',
+ eraseGrid);
